8adc8a9e1b6456eddd85365ff6b5aa9c88f58603
James-Gamboa <jjguevarag@gmail.com>
2022-08-18 14:01:08 -0600

    arregle la logica

--- js/conecta4.js
+++ js/conecta4.js
@@ -21,36 +21,45 @@ function transpose() {
   }
 }
 
+// @ts-ignore
 transpose(matriz);
 //darle click a la fila
+// @ts-ignore
 document.getElementById("1").addEventListener("click", () => {
   main(0);
 });
 
+// @ts-ignore
 document.getElementById("2").addEventListener("click", () => {
   main(1);
 });
 
+// @ts-ignore
 document.getElementById("3").addEventListener("click", () => {
   main(2);
 });
 
+// @ts-ignore
 document.getElementById("4").addEventListener("click", () => {
   main(3);
 });
 
+// @ts-ignore
 document.getElementById("5").addEventListener("click", () => {
   main(4);
 });
 
+// @ts-ignore
 document.getElementById("6").addEventListener("click", () => {
   main(5);
 });
 
+// @ts-ignore
 document.getElementById("7").addEventListener("click", () => {
   main(6);
 });
 
+// @ts-ignore
 document.getElementById("8").addEventListener("click", () => {
   main(7);
 });
@@ -59,8 +68,8 @@ document.getElementById("8").addEventListener("click", () => {
 function ganador(tablero, jugador, fila, columna) {
   // Aqui deben incluir el codigo para seleccionar cuando un usuario gana todas las posibles combinaciones.
 
-  // fila par, columna impar y se alterna (diagonales)
-  // misma fila, iniciando en pares e impares (horizontales)
+    fila par, columna impar y se alterna (diagonales)
+  misma fila, iniciando en pares e impares (horizontales)
   // diferente fila, misma columna (verticales)
 
   for (let i = 0; i < matriz.length; i++) {
@@ -183,7 +192,7 @@ function ganador() {
       }
     }
   }
-  return "none";
+  return "false";
 }
 
 if (matriz.length == 0) empate();
@@ -192,6 +201,7 @@ function empate() {
   console.log("Empate");
   alert("Empate");
   //TODO
+  // @ts-ignore
   pResultado.style.visibility = "visible";
 }
 
@@ -207,79 +217,108 @@ function main(fila) {
     return recursivoPila(vector, indexultimo - 1);
   }
 
+  // @ts-ignore
   ceromascercano = recursivoPila(matriz[fila], matriz.length);
 
+  // @ts-ignore
   valores1 = document.querySelectorAll("[id='1']");
+  // @ts-ignore
   valores2 = document.querySelectorAll("[id='2']");
+  // @ts-ignore
   valores3 = document.querySelectorAll("[id='3']");
+  // @ts-ignore
   valores4 = document.querySelectorAll("[id='4']");
+  // @ts-ignore
   valores5 = document.querySelectorAll("[id='5']");
+  // @ts-ignore
   valores6 = document.querySelectorAll("[id='6']");
+  // @ts-ignore
   valores7 = document.querySelectorAll("[id='7']");
+  // @ts-ignore
   valores8 = document.querySelectorAll("[id='8']");
 
   if (turno % 2 == 0) {
     switch (fila) {
       case 0:
+        // @ts-ignore
         valores1[ceromascercano].style.background = "yellow";
         break;
       case 1:
+        // @ts-ignore
         valores2[ceromascercano].style.background = "yellow";
         break;
       case 2:
+        // @ts-ignore
         valores3[ceromascercano].style.background = "yellow";
         break;
       case 3:
+        // @ts-ignore
         valores4[ceromascercano].style.background = "yellow";
         break;
       case 4:
+        // @ts-ignore
         valores5[ceromascercano].style.background = "yellow";
         break;
       case 5:
+        // @ts-ignore
         valores6[ceromascercano].style.background = "yellow";
         break;
       case 6:
+        // @ts-ignore
         valores7[ceromascercano].style.background = "yellow";
         break;
       case 7:
+        // @ts-ignore
         valores8[ceromascercano].style.background = "yellow";
         break;
     }
+    // @ts-ignore
     matriz[fila][ceromascercano] = 1;
   } else {
     switch (fila) {
       case 0:
+        // @ts-ignore
         valores1[ceromascercano].style.background = "blue";
         break;
       case 1:
+        // @ts-ignore
         valores2[ceromascercano].style.background = "blue";
         break;
       case 2:
+        // @ts-ignore
         valores3[ceromascercano].style.background = "blue";
         break;
       case 3:
+        // @ts-ignore
         valores4[ceromascercano].style.background = "blue";
         break;
       case 4:
+        // @ts-ignore
         valores5[ceromascercano].style.background = "blue";
         break;
       case 5:
+        // @ts-ignore
         valores6[ceromascercano].style.background = "blue";
         break;
       case 6:
+        // @ts-ignore
         valores7[ceromascercano].style.background = "blue";
         break;
       case 7:
+        // @ts-ignore
         valores8[ceromascercano].style.background = "blue";
         break;
     }
+    // @ts-ignore
     matriz[fila][ceromascercano] = 2;
   }
 
+  // @ts-ignore
   validacion(matriz, (turno % 2) + 1, fila, ceromascercano);
   turno++;
 }
 function validacion(matriz, turno, fila, columna) {
+  // @ts-ignore
   let hayganador = ganador(matriz, turno, fila, columna);
   if (hayganador) {
     alert("El jugador " + turno + " gana!");
